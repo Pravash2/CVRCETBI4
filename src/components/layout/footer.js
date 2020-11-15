65f73ef76c3402f96c3eb73cc6c29809e14f14f6
PRAVASH KUMAR BEHERA <40057758+Pravash2@users.noreply.github.com>
2020-11-15 23:44:14 +0530

    Update footer.js

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -155,7 +155,7 @@ class Footer extends React.Component {
                   <p>Copyright @2019 CVRCE TBI. All rights reserved</p>
                   <p>
                     Curated with <i className="fa fa-heart"></i> by{" "}
-                    <a href="http://www.pravash.tech" target="_blank">
+                    <a href="https://www.pravash.me" target="_blank">
                       {" "}
                       <span>Pravash</span>
                     </a>
